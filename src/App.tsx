c08c4fe17b39817ac985705990d62cafd2f6735b
junya-thinkactive-d <develop.junya@gmail.com>
2023-01-03 17:25:04 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Sample from './components/Sample';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello! Three.js</h1>
+        <Sample />
+      </div>
+    </>
+  );
+}
+
+export default App;
